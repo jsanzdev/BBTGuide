9783917e4400d10f315eeb8844c93ab857369841
Jesus Sanz <jesus@jsanz.dev>
2022-12-03 11:46:57 +0100

    Initial Commit

---
+++ BBTGuide/BBTGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBTGuideApp.swift
+//  BBTGuide
+//
+//  Created by Jesus Sanz on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BBTGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
